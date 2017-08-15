009da168157da01902b54683ba34a8ab56be0971
Alex Luna <alexluna@Alexs-MacBook-Air.local>
2017-08-15 16:05:12 -0400

    jasmine enzyme spec test

--- dog-twitter-with-enzyme/test/components/TweetSpec.js
+++ dog-twitter-with-enzyme/test/components/TweetSpec.js
@@ -3,5 +3,51 @@ import Tweet from '../../src/components/Tweet';
 describe('Tweet', () => {
 
   // YOUR TEST CODE HERE
+  let text,
+      name,
+      userPhoto,
+      className,
+      wrapper;
 
-})
+  beforeEach(() => {
+    wrapper = mount(
+      <Tweet
+        text= "This doesn't look like Kansas"
+        name= "Todo"
+        userPhoto= "http://images2.fanpop.com/image/quiz/400000/400752_1271413686448_431_300.jpg"
+        className= "selected"
+      />
+    );
+  });
+
+  it('should render a div tag', () => {
+    expect(wrapper.find('div')).toBePresent();
+  });
+
+  it('should render an img tag', () => {
+    expect(wrapper.find('img')).toBePresent();
+  });
+
+  it('should render a span tag', () => {
+    expect(wrapper.find('span')).toBePresent();
+  });
+
+  it('should render a p tag', () => {
+    expect(wrapper.find('p')).toBePresent();
+  });
+
+  it('should have a span tag with the correct text', () => {
+    expect(wrapper.find('span').text()).toBe("Todo");
+  });
+
+  it('should have an img tag with the correct image', () => {
+    expect(wrapper.find('img').props()).toEqual({
+      src: "http://images2.fanpop.com/image/quiz/400000/400752_1271413686448_431_300.jpg"
+    });
+  });
+
+  it('should have a p tag with the correct text', () => {
+    expect(wrapper.find('p').text()).toBe("This doesn't look like Kansas");
+  });
+
+});
--- dog-twitter-with-enzyme/test/containers/TwitterFeedSpec.js
+++ dog-twitter-with-enzyme/test/containers/TwitterFeedSpec.js
@@ -21,8 +21,24 @@ describe('TwitterFeed', () => {
   });
 
   //YOUR TEST CODE HERE
-  it('should return true', () => {
-    expect(true).toEqual(true);
+  it('should have a specified initial state', () => {
+    expect(wrapper.state()).toEqual({ selectedTweetId: null });
+  });
+
+  it('should render an Tweet Component', () => {
+    expect(wrapper.find(Tweet)).toBePresent();
+  });
+
+  it('should change the tweet props if state id is changed', () => {
+    wrapper.setState({ selectedTweetId: 1 });
+    expect(wrapper.find(Tweet).props()).toEqual({
+      id: 1,
+      text: "This doesn't look like Kansas",
+      name: "Todo",
+      userPhoto: 'http://images2.fanpop.com/image/quiz/400000/400752_1271413686448_431_300.jpg',
+      handleClick: jasmine.any(Function),
+      className: "selected"
+    })
   })
 
 })
