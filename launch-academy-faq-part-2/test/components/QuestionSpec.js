eafbcec0e861b9739111e26eba54bd0d30c414a1
Alex Luna <alexluna@Alexs-MacBook-Air.local>
2017-08-16 16:12:48 -0400

    refactor test

--- launch-academy-faq-part-2/test/components/QuestionSpec.js
+++ launch-academy-faq-part-2/test/components/QuestionSpec.js
@@ -6,16 +6,25 @@ describe('Question', () => {
 
   beforeEach(() => {
 
-    onClickSpy = jasmine.createSpy('onClick Spy')
+    onClickSpy = jasmine.createSpy('on click handler Spy')
 
     wrapper = mount(
       <Question
         question={'What is your camp like?'}
-        answer={'it\'s great'}
+        answer={"it's great"}
         selected={true}
         handleClick={onClickSpy}
       />
     )
+  })
+
+  it('should render a Question component that has the given props', () => {
+    expect(wrapper.props()).toEqual({
+      question: 'What is your camp like?',
+      answer: "it's great",
+      selected: true,
+      handleClick: onClickSpy
+    })
   })
 
   it('should render a Question component that has an h5 with question text and onClick function', () => {
@@ -27,7 +36,7 @@ describe('Question', () => {
 
   it('should render a Question component that has a p tag with answer text', () => {
     expect(wrapper.find('p')).toBePresent()
-    expect(wrapper.find('p').text()).toEqual('it\'s great')
+    expect(wrapper.find('p').text()).toEqual("it's great")
   })
 
   it('should render a Question component that has an i tag with answer text that has a click function', () => {
@@ -44,5 +53,29 @@ describe('Question', () => {
     expect(wrapper.find('div').at(1)).toHaveProp('className', 'unselected-question')
   })
 
+
+})
+
+describe('Question', () => {
+  let wrapper;
+  let onClickSpy;
+
+  beforeEach(() => {
+
+    onClickSpy = jasmine.createSpy('on click handler Spy')
+
+    wrapper = mount(
+      <Question
+        question={'What is your camp like?'}
+        answer={"it's great"}
+        selected={false}
+        handleClick={onClickSpy}
+      />
+    )
+  })
+
+  it('should not render the answer', () => {
+    expect(wrapper.find('p').text()).toEqual("")
+  })
 
 })
