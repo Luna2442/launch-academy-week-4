51f2778452872f0a7377f5382b793ea87fab38e3
Alex Luna <alexluna@Alexs-MacBook-Air.local>
2017-08-16 16:16:18 -0400

    further refactor

--- launch-academy-faq-part-2/test/components/QuestionSpec.js
+++ launch-academy-faq-part-2/test/components/QuestionSpec.js
@@ -4,78 +4,77 @@ describe('Question', () => {
   let wrapper;
   let onClickSpy;
 
-  beforeEach(() => {
+  describe('Question is selected', () => {
+    beforeEach(() => {
 
-    onClickSpy = jasmine.createSpy('on click handler Spy')
+      onClickSpy = jasmine.createSpy('on click handler Spy')
 
-    wrapper = mount(
-      <Question
-        question={'What is your camp like?'}
-        answer={"it's great"}
-        selected={true}
-        handleClick={onClickSpy}
-      />
-    )
-  })
-
-  it('should render a Question component that has the given props', () => {
-    expect(wrapper.props()).toEqual({
-      question: 'What is your camp like?',
-      answer: "it's great",
-      selected: true,
-      handleClick: onClickSpy
+      wrapper = mount(
+        <Question
+          question={'What is your camp like?'}
+          answer={"it's great"}
+          selected={true}
+          handleClick={onClickSpy}
+        />
+      )
     })
+
+    it('should render a Question component that has the given props', () => {
+      expect(wrapper.props()).toEqual({
+        question: 'What is your camp like?',
+        answer: "it's great",
+        selected: true,
+        handleClick: onClickSpy
+      })
+    })
+
+    it('should render a Question component that has an h5 with question text and onClick function', () => {
+      expect(wrapper.find('h5')).toBePresent()
+      expect(wrapper.find('h5').text()).toEqual('What is your camp like?')
+      wrapper.find('h5').simulate('click')
+      expect(onClickSpy).toHaveBeenCalled();
+    })
+
+    it('should render a Question component that has a p tag with answer text', () => {
+      expect(wrapper.find('p')).toBePresent()
+      expect(wrapper.find('p').text()).toEqual("it's great")
+    })
+
+    it('should render a Question component that has an i tag with answer text that has a click function', () => {
+      expect(wrapper.find('i')).toBePresent()
+      wrapper.find('i').simulate('click')
+      expect(onClickSpy).toHaveBeenCalled();
+    })
+
+
+    it('should render a Question component that has a div tag with correct class name', () => {
+      expect(wrapper.find('div').at(1)).toBePresent()
+      expect(wrapper.find('div').at(1)).toHaveProp('className', 'selected-question')
+      wrapper.setProps({ selected: false });
+      expect(wrapper.find('div').at(1)).toHaveProp('className', 'unselected-question')
+    })
+
   })
 
-  it('should render a Question component that has an h5 with question text and onClick function', () => {
-    expect(wrapper.find('h5')).toBePresent()
-    expect(wrapper.find('h5').text()).toEqual('What is your camp like?')
-    wrapper.find('h5').simulate('click')
-    expect(onClickSpy).toHaveBeenCalled();
-  })
+  describe('Question is not selected', () => {
+    beforeEach(() => {
 
-  it('should render a Question component that has a p tag with answer text', () => {
-    expect(wrapper.find('p')).toBePresent()
-    expect(wrapper.find('p').text()).toEqual("it's great")
-  })
+      onClickSpy = jasmine.createSpy('on click handler Spy')
 
-  it('should render a Question component that has an i tag with answer text that has a click function', () => {
-    expect(wrapper.find('i')).toBePresent()
-    wrapper.find('i').simulate('click')
-    expect(onClickSpy).toHaveBeenCalled();
-  })
+      wrapper = mount(
+        <Question
+          question={'What is your camp like?'}
+          answer={"it's great"}
+          selected={false}
+          handleClick={onClickSpy}
+        />
+      )
+    })
 
+    it('should not render the answer', () => {
+      expect(wrapper.find('p').text()).toEqual("")
+    })
 
-  it('should render a Question component that has a div tag with correct class name', () => {
-    expect(wrapper.find('div').at(1)).toBePresent()
-    expect(wrapper.find('div').at(1)).toHaveProp('className', 'selected-question')
-    wrapper.setProps({ selected: false });
-    expect(wrapper.find('div').at(1)).toHaveProp('className', 'unselected-question')
-  })
-
-
-})
-
-describe('Question', () => {
-  let wrapper;
-  let onClickSpy;
-
-  beforeEach(() => {
-
-    onClickSpy = jasmine.createSpy('on click handler Spy')
-
-    wrapper = mount(
-      <Question
-        question={'What is your camp like?'}
-        answer={"it's great"}
-        selected={false}
-        handleClick={onClickSpy}
-      />
-    )
-  })
-
-  it('should not render the answer', () => {
-    expect(wrapper.find('p').text()).toEqual("")
   })
 
 })
